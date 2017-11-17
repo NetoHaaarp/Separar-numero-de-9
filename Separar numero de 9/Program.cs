5d04c1b5d4b8dcad19e07405ef0c0346270b7c0f
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-16 23:06:25 -0600

    Agregar archivos de proyecto.

---
+++ Separar numero de 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Separar_numero_de_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero de 9 cifras");
+            string n = Console.ReadLine();
+            string[] cad = new string[9];
+            for (int i = 0; i < cad.Length; i++)
+                cad[i] = n[i] + "";
+            foreach (string item in cad)
+                Console.Write(item + ", ");
+            Console.ReadKey();
+
+        }
+    }
+}
